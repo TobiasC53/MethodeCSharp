ad8f6e6a2b7a985fe8d065dc196d3045fd87aed8
Tobias Cremer <tobias.c11@gso.schule.koeln>
2022-03-23 12:09:15 +0100

    Projektdateien hinzufügen.

---
+++ Methode Aufgabe 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Methode_Aufgabe_1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int anzahl = 0;
+
+
+            Console.WriteLine("Bitte geben sie eine Zahl ein:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Geben sie ihren Text ein: ");
+            string text = Convert.ToString(Console.ReadLine());
+
+            anzahl = ausgabeTextMalX(text, x);
+
+            Console.Write("\nAnzahl der Buchstaben: {0}", anzahl);
+        }
+
+        static int ausgabeTextMalX(string text, int x)
+        {
+
+            int textgroesse = text.Length;
+            int y = textgroesse * x;
+            return y;
+        }
+    }
+}
